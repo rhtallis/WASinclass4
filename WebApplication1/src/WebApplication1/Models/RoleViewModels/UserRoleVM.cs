311bb386949c24e8377dfecb2ce95efa6017b29a
Tony Shum <tony.kin.shum@gmail.com>
2017-02-16 16:04:17 -0800

    Implemented AddUserToRole and added authorization checks

---
+++ WebApplication1/src/WebApplication1/Models/RoleViewModels/UserRoleVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.RoleViewModels
+{
+    public class UserRoleVM
+    {
+        [Required]
+        [Display(Name = "Login Name")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
